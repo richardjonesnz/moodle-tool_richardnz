2ded8a9f595bf2686658f758ddd4724b7b578c45
Richard Jones <richard@ib-computing.com>
2018-12-19 14:08:24 +1300

    Muddled my way to this solution

--- amd/src/alert.js
+++ amd/src/alert.js
@@ -1,19 +1,19 @@
 define(['jquery', 'core/log', 'core/ajax'], function($, log) {
     "use strict";
-    log.debug('alert js module loaded');
+    //log.debug('alert js module loaded');
     return {
         init: function(return_url) {
-            log.debug('alert js init');
+            //log.debug('alert js init');
             $('a.tool_richardnz_deletelink').click(function(e) {
-                log.debug('alert link clicked');
+                //log.debug('alert link clicked');
                 if(!confirm('Are you sure you want to delete?')) {
                     e.preventDefault();
-                    log.debug('alert link cancelled');
+                    //log.debug('alert link cancelled');
                     $.ajax({
                         url:return_url,
                     });
                 } else {
-                    log.debug('alert link confirmed');
+                    //log.debug('alert link confirmed');
                 }
             });
         }
